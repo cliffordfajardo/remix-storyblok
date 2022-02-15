e1faf0ec67ec670871ff17442393098ffa631a59
Stephen Wilde <kink@mandys.house>
2022-02-15 18:41:37 +0000

    Remove import sorting until fixed with tailwind sorting

--- app/components/storyblok/utilities/placeholder/index.jsx
+++ app/components/storyblok/utilities/placeholder/index.jsx
@@ -1,6 +1,6 @@
 const StoryblokHeadingText = ({ content }) => {
     return (
-        <div className="alert rounded-box mb-2 bg-neutral text-neutral-content shadow-lg">
+        <div className="rounded-box mb-2 bg-neutral alert text-neutral-content shadow-lg">
             <div className="flex-1 justify-center">
                 <label>
                     The component <span className="font-bold">{content}</span>{" "}
--- prettier.config.js
+++ prettier.config.js
@@ -1,16 +1,4 @@
 module.exports = {
-    importOrder: [
-        "react",
-        "remix",
-        "graphql",
-        "components",
-        "utilities",
-        "^[./]",
-        "<THIRD_PARTY_MODULES>",
-    ],
-    importOrderSeparation: true,
-    importOrderSortSpecifiers: true,
-    importOrderCaseInsensitive: true,
     semi: true,
     singleQuote: false,
     tabWidth: 4,
